c7dbb874885b1670160bdb06db27953a4d72d55e
marizoo <marizoo@meow.woof>
2021-12-08 00:28:44 +0700

    completed components of navbar, and  announcement

---
+++ src/components/Slider.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const Slider = () => {
+    return (
+        <Container>
+            slide baby
+        </Container>
+    )
+}
+
+export default Slider
+
+const Container = styled.div``
