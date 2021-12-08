cacd5b390316db944e3e3c29360ef57cb06d0fb5
marizoo <marizoo@meow.woof>
2021-12-09 00:07:06 +0700

    completed all components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route} from "react-router-dom";
 import Cart from "./pages/Cart";
 import Home from './pages/Home';
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+import ProductPg from "./pages/ProductPg";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -10,6 +14,18 @@ const App = () => {
       </Routes>
       <Routes>
         <Route path="/cart" element={<Cart />} /> 
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} /> 
+      </Routes>
+      <Routes>
+        <Route path="/register" element={<Register />} /> 
+      </Routes>
+      <Routes>
+        <Route path="/product" element={<ProductPg />} /> 
+      </Routes>
+      <Routes>
+        <Route path="/productlist" element={<ProductList />} /> 
       </Routes>
   </>
   )};
