a531ad44720c1ef271081fd28b3fbe28316421a8
marizoo <marizoo@meow.woof>
2021-12-08 22:55:41 +0700

    page: Cart : done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import {} from 'react-router-dom'
+import { Routes, Route} from "react-router-dom";
+import Cart from "./pages/Cart";
 import Home from './pages/Home';
 
 const App = () => {
   return (
-  <div>
-    <Home />
-  </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+      </Routes>
+      <Routes>
+        <Route path="/cart" element={<Cart />} /> 
+      </Routes>
+  </>
   )};
+
+  
 
 export default App;
 
